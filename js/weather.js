143fa56e6a691c8ef9f29fb0fb8c85fba28aa734
Sensol <80695040+SoongGaktal@users.noreply.github.com>
2021-07-20 23:35:54 +0900

    Night weather icon add

--- js/weather.js
+++ js/weather.js
@@ -4,11 +4,20 @@ let weatherIcon = {
     '02d' : 'fa-cloud-sun',
     '03d' : 'fa-cloud',
     '04d' : 'fa-cloud-meatball',
-    '09d' : 'fa-cloud-sun-rain',
+    '10n' : 'fa-cloud-sun-rain',
     '10d' : 'fa-cloud-showers-heavy',
     '11d' : 'fa-poo-storm',
     '13d' : 'fa-snowflake',
-    '50d' : 'fa-smog' 
+    '50d' : 'fa-smog',
+    '01n' : 'fa-sun',
+    '02n' : 'fa-cloud-sun',
+    '03n' : 'fa-cloud',
+    '04n' : 'fa-cloud-meatball',
+    '10n' : 'fa-cloud-sun-rain',
+    '10n' : 'fa-cloud-showers-heavy',
+    '11n' : 'fa-poo-storm',
+    '13n' : 'fa-snowflake',
+    '50n' : 'fa-smog' 
 };
 
 function onGeoOk(position) {
@@ -22,8 +31,9 @@ function onGeoOk(position) {
         const icon = document.querySelector("#weather > i");
         const weather = document.querySelector("#weather > span:nth-child(2)");
         const city = document.querySelector("#weather > span:nth-child(4)");
-        
         const iconIndex =  data.weather[0].icon;
+        console.log(iconIndex);
+
         icon.classList.add('fas', weatherIcon[iconIndex]);
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
         city.innerText = data.name;
